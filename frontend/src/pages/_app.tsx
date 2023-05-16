e6808e0d4d3834746c016e19821414820010adcb
Mo Arefi <m-arefi@live.com>
2023-05-16 09:40:02 +0200

    Updated package.json, removed auth0

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-import { AUTH0_CONFIG } from "../../utils/auth0-config";
 import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import "../styles/tailwind.scss";
@@ -12,14 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   };
 
   return (
-    <Auth0Provider
-      domain={AUTH0_CONFIG.domain}
-      clientId={AUTH0_CONFIG.clientId}
-      redirectUri={AUTH0_CONFIG.redirectUri}
-      onRedirectCallback={onRedirectCallback}
-    >
-      <Component {...pageProps} />
-    </Auth0Provider>
+    <Component {...pageProps} onRedirectCallback={onRedirectCallback} />
   );
 }
 
