8f6c2901f6bca8165c780ce60dc2ba70ddf7d3aa
Mo Arefi <m-arefi@live.com>
2023-05-16 09:44:24 +0200

    Dev functional with updated package.json

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-import { AUTH0_CONFIG } from "../../utils/auth0-config";
 import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import "../styles/tailwind.scss";
@@ -12,14 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   };
 
   return (
-    <Auth0Provider
-      domain={AUTH0_CONFIG.domain}
-      clientId={AUTH0_CONFIG.clientId}
-      redirectUri={AUTH0_CONFIG.redirectUri}
+    <Component
+      {...pageProps}
       onRedirectCallback={onRedirectCallback}
-    >
-      <Component {...pageProps} />
-    </Auth0Provider>
+    />
   );
 }
 
