5a81924e27736e8458021bee20bd50a62ec53afc
Elizabethucar <elizg77@hotmail.com>
2023-05-23 10:30:18 +0200

    added description

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -98,9 +98,9 @@ export function Navbar() {
                 </button>
                 <Link  href="/login">
                   <a
-			class="inline-block mt-2 bg-green-600 hover:bg-green-700 focus:bg-green-800 px-6 py-2 rounded text-white shadow-lg"
-		>
-			Login </a></Link>
+			         class="inline-block mt-2 bg-green-600 hover:bg-green-700 focus:bg-green-800 px-6 py-2 rounded-full text-white shadow-lg"
+		            >
+			          Login </a></Link>
 		
               </div>
             </div>
@@ -111,14 +111,14 @@ export function Navbar() {
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="/"
                 className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
               >
                 Home
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="/products"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
               >
                 Catalogue
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Head from 'next/head';
 import { Hero } from "../components/Hero";
 import { ProductTrio } from "../components/ProductTrio";
 import Carousel from "../components/Carousel";
@@ -11,6 +11,13 @@ import { Newsletter } from "../components/Newsletter";
 export default function Home() {
   return (
     <div className="">
+        <Head>
+        <title>MESL - Only Clothing Store You'll Ever Need!</title>
+        <meta
+          name="description"
+          content="We strive to provide the best quality clothing to our clients."
+        />
+      </Head>
   
       <main>
         <Hero />
