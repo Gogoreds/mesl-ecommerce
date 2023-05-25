4b00a2e943d1a82a42ad230424b3ac8fa0360870
Mohammad Arefi <m-arefi@live.com>
2023-05-25 23:30:41 +0200

    ProductTrio working like a charm

--- frontend/src/components/ProductTrio.jsx
+++ frontend/src/components/ProductTrio.jsx
@@ -1,10 +1,7 @@
-export function ProductTrio({ product }) {
-  if (!product || !product.fields) {
-    return null; // Handle the case when product or product.fields is undefined
-  }
+import React from "react";
+import ProductCard from "./ProductCard";
 
-  const { title, slug, images, price, category } = product.fields;
-
+const ProductTrio = ({ products }) => {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
@@ -22,28 +19,8 @@ export function ProductTrio({ product }) {
         </div>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8">
-          {images.map((image) => (
-            <div key={image.id} className="group relative">
-              <div className="h-96 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2 group-hover:opacity-75 sm:h-auto">
-                <img
-                  src={image.fields.file.url}
-                  alt={image.fields.title}
-                  className={classNames(
-                    image.primary
-                      ? "lg:col-span-2 lg:row-span-2"
-                      : "hidden lg:block",
-                    "rounded-lg"
-                  )}
-                />
-              </div>
-              <h3 className="mt-4 text-base font-semibold text-gray-900">
-                <a href={slug}>
-                  <span className="absolute inset-0" />
-                  {title}
-                </a>
-              </h3>
-              <p className="mt-1 text-sm text-gray-500">{price}</p>
-            </div>
+          {products.map((product) => (
+            <ProductCard key={product.sys.id} product={product} />
           ))}
         </div>
 
@@ -59,4 +36,6 @@ export function ProductTrio({ product }) {
       </div>
     </div>
   );
-}
+};
+
+export default ProductTrio;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { createClient } from "contentful";
 import { Hero } from "../components/Hero";
-import { ProductTrio } from "../components/ProductTrio";
+import ProductTrio from "../components/ProductTrio";
 import Carousel from "../components/Carousel";
-import Category from "../components/Category";
 import { Blog } from "../components/Blog";
 import { Newsletter } from "../components/Newsletter";
 
-export default function Home({ product }) {
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const client = createClient({
+      space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE,
+      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    });
+
+    const fetchProducts = async () => {
+      try {
+        const response = await client.getEntries({ content_type: "product" });
+        setProducts(response.items);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="">
       <main>
         <Hero />
         <Carousel />
-        <ProductTrio product={product} />
-        <Category product={product} />
+        <ProductTrio products={products} />
         <Blog />
         <Newsletter />
       </main>
