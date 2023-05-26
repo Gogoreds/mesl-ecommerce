64e5d29f42529fea96d8188bf604f6b6e5aff093
Elizabeth <Elizabeth@sudosweden.com>
2023-05-26 17:41:52 +0200

    categories done, nedds css

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,4 +1,75 @@
 import Image from "next/image";
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  });
+  const res = await client.getEntries({ content_type: "category" });
+  return {
+    props: {
+      category: res.items,
+    },
+  };
+}
+
+const Category = ({ category }) => {
+  if (!category || !category.length) {
+    return null;
+  }
+
+  return (
+    <div className="bg-gray-50">
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+        {/* Rest of your code */}
+       <div className="sm:flex sm:items-baseline sm:justify-between">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Shop by Category
+          </h2>
+          <a
+            href="/products"
+            className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block"
+          >
+            Browse all categories
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+          {category.slice(0, 3).map((cat) => {
+            const { image } = cat.fields;
+
+            const imageUrl = Array.isArray(image)
+              ? "https:" + image[0].fields.file.url
+              : "https:" + image.fields.file.url;
+
+            return (
+              <div
+                key={cat.sys.id}
+                className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2"
+              >
+                <Image
+                  src={imageUrl}
+                  layout="fill"
+                  className="h-full w-full object-cover object-center"
+                />
+                {/* Rest of your code */}
+              </div>
+            );
+          })}
+        </div>
+        {/* Rest of your code */}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
+
+
+/* 
+
+/* 
 
 const Category = ({ product }) => {
   if (!product || !product.fields) {
@@ -115,4 +186,4 @@ const Category = ({ product }) => {
     </div>
   );
 };
-export default Category;
+export default Category; */
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,9 +5,11 @@ import ProductTrio from "../components/ProductTrio";
 import Carousel from "../components/Carousel";
 import { Blog } from "../components/Blog";
 import { Newsletter } from "../components/Newsletter";
+import Category from '../components/Category'
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [categorys, setCategorys] = useState([]);
 
   useEffect(() => {
     const client = createClient({
@@ -23,8 +25,17 @@ export default function Home() {
         console.error("Error fetching products:", error);
       }
     };
+    const fetchCategory = async () => {
+      try {
+        const response = await client.getEntries({ content_type: "category" });
+        setCategorys(response.items);
+      } catch (error) {
+        console.error("Error fetching category:", error);
+      }
+    };
 
     fetchProducts();
+    fetchCategory()
   }, []);
 
   return (
@@ -33,6 +44,7 @@ export default function Home() {
         <Hero />
         <Carousel />
         <ProductTrio products={products} />
+        <Category  category={categorys}  />
         <Blog />
         <Newsletter />
       </main>
