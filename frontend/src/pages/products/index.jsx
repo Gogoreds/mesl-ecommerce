918f0a9fbdad124440aadba543c7b5c9e806859f
Elizabethucar <elizg77@hotmail.com>
2023-05-29 22:10:42 +0200

    searchbar in product done

--- frontend/src/pages/products/index.jsx
+++ frontend/src/pages/products/index.jsx
@@ -1,7 +1,7 @@
 import { createClient } from "contentful";
 import ProductCard from "../../components/ProductCard";
+import React, { useState } from "react";
 
-//connection to contentful
 export async function getStaticProps() {
   const client = createClient({
     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE,
@@ -16,13 +16,36 @@ export async function getStaticProps() {
 }
 
 const ProductList = ({ products }) => {
+  const [search, setSearch] = useState("");
+
   console.log(products);
+
+  const filteredProducts = products.filter((product) => {
+    if (search === "") {
+      return true;
+    } else if (
+      product.fields.title &&
+      product.fields.title.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
   return (
     <div className="bg-white">
       <div className="sm:flex sm:items-baseline sm:justify-between">
         <h2 className="mx-10 mt-12 text-2xl font-bold tracking-tight text-gray-900">
           Our Shop
         </h2>
+        <input
+          type="text"
+          placeholder="Search product..."
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
         <a
           href="/"
           className="mx-10 hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block"
@@ -32,9 +55,13 @@ const ProductList = ({ products }) => {
         </a>
       </div>
       <div className="mt-0 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8">
-        {products.map((product) => (
-          <ProductCard key={product.sys.id} product={product} />
-        ))}
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <ProductCard key={product.sys.id} product={product} />
+          ))
+        ) : (
+          <p>No products found.</p>
+        )}
       </div>
     </div>
   );
