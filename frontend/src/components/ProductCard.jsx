0faf4287efacf0e1e1135c5e4fcc34124526a704
Elizabethucar <elizg77@hotmail.com>
2023-05-30 12:38:40 +0200

    added react icon

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,23 @@
 import Image from "next/image";
+import React, { useState, useEffect } from 'react';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 const ProductCard = ({ product }) => {
   const { title, slug, image, price, category } = product.fields;
+  const [like, setLike] = useState(false);
+
+  const toggleLike = () => {
+    const updatedLike = !like;
+    setLike(updatedLike);
+    localStorage.setItem(`liked_${slug}`, updatedLike.toString());
+  };
+
+  useEffect(() => {
+    const likedState = localStorage.getItem(`liked_${slug}`);
+    if (likedState) {
+      setLike(likedState === 'true');
+    }
+  }, [slug]);
 
   return (
     <div className="bg-white">
@@ -22,15 +38,19 @@ const ProductCard = ({ product }) => {
           </h3>
           <h4 className="mt-1 text-sm text-gray-500">Category: {category}</h4>
           <p className="mt-1 text-sm text-gray-500">{price} Kr</p>
+          <p onClick={toggleLike}>
+            {like ? <FaHeart className='absolute top-4 left-4 text-gray-300'/> : <FaRegHeart className='absolute top-4 left-4 text-gray-300'/>}
+          </p>
           <button
-                  type="submit"
-                  className="mt-8 flex w-full items-center justify-center rounded-full border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Add to cart
-                </button>
+            type="submit"
+            className="mt-8 flex w-full items-center justify-center rounded-full border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
   );
 };
+
 export default ProductCard;
