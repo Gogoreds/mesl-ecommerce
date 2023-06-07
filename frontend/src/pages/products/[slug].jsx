4e9f6b13222955fd08f5299121242ffd20cf63e5
ezelnali <lihemy@live.com>
2023-06-07 12:39:39 +0200

    finally

--- frontend/src/pages/products/[slug].jsx
+++ frontend/src/pages/products/[slug].jsx
@@ -79,6 +79,13 @@ const Details = ({ product }) => {
               <ProductCard key={product.sys.id} product={product} />
   
       </main>
+
+<button
+                  type="submit"
+                  className="mt-8 flex w-full items-center justify-center rounded-full border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Add to cart
+                </button>
     </div>
     </div>
   );
