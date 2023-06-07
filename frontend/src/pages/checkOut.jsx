4aaf0c0ae223c128100f223c16621aeb62aacb94
Sara Razm <razmsara@gmail.com>
2023-06-07 15:19:28 +0200

    Wrapped checkout in Elements

--- frontend/src/pages/checkOut.jsx
+++ frontend/src/pages/checkOut.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { CheckOut } from "../components/CheckOut";
+
+const stripePromise = loadStripe(
+  "pk_test_51NBrxZExwc4I2AGOECdsJI1NQzZ3lgzwjFpW57EFWFT5ngnu30J0kQoZDWj6fkEydSzVtFdg9TrdlCmXDlQrV5jA00hskiwfLZ"
+);
 
 export default function Home() {
   return (
-    <div className="">
+    <Elements stripe={stripePromise}>
       <main>
         <CheckOut />
       </main>
-    </div>
+    </Elements>
   );
 }
